cd3e83dba84f65aefe5bd11bc32ece0d396b3f97
Ruotian Zhang <zrtqwq@gmail.com>
2019-05-16 22:47:05 -0500

    add create api

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"sync"
 
 	"github.com/labstack/echo"
@@ -11,12 +12,18 @@ import (
 var (
 	secretkey      = CONFIG_SecretKey
 	publickey      = CONFIG_PublicKey
+	disquskey      = "E8Uh5l5fHZ6gD8U3KycjAIAk46f68Zw7C6eW8WSjZvCLXebZ7p0r1yrYDrLilk2F"
 	shortname      = CONFIG_ShortName
 	maxCachedLimit = 100000
 	listPostsApi   = "https://disqus.com/api/3.0/threads/listPosts.json"
 	listPostsLimit = "100"
+	createPostApi  = "https://disqus.com/api/3.0/posts/create.json"
+	listThreadApi  = "https://disqus.com/api/3.0/threads/list.json"
+	approvePostApi = "https://disqus.com/api/3.0/posts/approve.json"
 	cached         map[string][]byte
 	lock           sync.RWMutex
+	defaultMail    = "bot@yeah.moe"
+	defaultName    = "Guest"
 )
 
 func updatelink(link string) []byte {
@@ -54,11 +61,58 @@ func handleListPosts(c echo.Context) error {
 	}
 }
 
+// /listThreads
+func handleListThreads(c echo.Context) error {
+	link := c.QueryParam("link")
+	if link == "" {
+		return c.JSONBlob(http.StatusOK, []byte("{\"result\": \"please provide link\"}"))
+	}
+	resp, err := http.Get(listThreadApi + "?api_key=" + publickey + "&forum=" + shortname + "&thread=link:" + link)
+	if err != nil {
+		return c.JSONBlob(http.StatusOK, []byte("{\"result\": \"failed to access to disqus api\"}"))
+	}
+	defer resp.Body.Close()
+	encodedJSON, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return c.JSONBlob(http.StatusOK, []byte("{\"result\": \"failed to read body\"}"))
+	}
+	return c.JSONBlob(http.StatusOK, encodedJSON)
+}
+
+// /create
+func handleCreatePost(c echo.Context) error {
+	thread := c.FormValue("thread")
+	message := c.FormValue("message")
+	name := c.FormValue("name")
+	if thread == "" {
+		return c.JSONBlob(http.StatusOK, []byte("{\"result\": \"please provide thread\"}"))
+	}
+	if message == "" {
+		return c.JSONBlob(http.StatusOK, []byte("{\"result\": \"please provide message\"}"))
+	}
+	if name == "" {
+		name = defaultName
+	}
+	resp, err := http.PostForm(createPostApi,
+		url.Values{"thread": {thread}, "message": {message}, "author_name": {name}, "author_email": {defaultMail}, "api_key": {disquskey}})
+	if err != nil {
+		return c.JSONBlob(http.StatusOK, []byte("{\"result\": \"failed to access to disqus api\"}"))
+	}
+	defer resp.Body.Close()
+	encodedJSON, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return c.JSONBlob(http.StatusOK, []byte("{\"result\": \"failed to read body\"}"))
+	}
+	return c.JSONBlob(http.StatusOK, encodedJSON)
+}
+
 func main() {
 	cached = make(map[string][]byte)
 	e := echo.New()
 
 	e.GET("/listPosts", handleListPosts)
+	e.GET("/listThreads", handleListThreads)
+	e.POST("/create", handleCreatePost)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "ok")
 	})
